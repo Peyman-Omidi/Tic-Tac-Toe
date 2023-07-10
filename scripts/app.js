5674877a5f89f8bb9917e622308cd7b444dfd327
Peyman Omidi <pomidi827@gmail.com>
2023-07-10 11:37:55 +0330

    validating user input

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,13 @@ const backDropElement = document.getElementById('backdrop')
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const formElement = document.querySelector('form');
+const errorOutputElement = document.getElementById('config-errors')
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backDropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig)
--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,23 @@
 function openPlayerConfig() {
-  playerConfigOverlayElement.style.display = 'block';
-  backDropElement.style.display = 'block';
+  playerConfigOverlayElement.style.display = "block";
+  backDropElement.style.display = "block";
 }
 
 function closePlayerConfig() {
-  playerConfigOverlayElement.style.display = 'none';
-  backDropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = "none";
+  backDropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove('error')
+  errorOutputElement.textContent = ''
+}
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+  if (!enteredPlayerName) {
+    // enteredPlayedName === ""
+
+    errorOutputElement.textContent = "please enter a valid name!";
+    event.target.firstElementChild.classList.add('error')
+    return;
+  }
 }
